afa959ebdb8638879518915029251cfe82fa4dc5
mostafaitalian <eng_mustafa_yossef@hotmail.com>
2021-05-21 05:41:17 +0200

    fikkk commit data

---
+++ build/precache-manifest.53b93851871ee1ef2b4cf0ac55b672f5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f0ccc89d832cd26c5c29db30769860c",
+    "url": "/wouldyourather/index.html"
+  },
+  {
+    "revision": "1bdb3941ff86d84907cb",
+    "url": "/wouldyourather/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "37c3478a9a005ed15947",
+    "url": "/wouldyourather/static/css/main.a60bb65d.chunk.css"
+  },
+  {
+    "revision": "1bdb3941ff86d84907cb",
+    "url": "/wouldyourather/static/js/2.e823ac98.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/wouldyourather/static/js/2.e823ac98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37c3478a9a005ed15947",
+    "url": "/wouldyourather/static/js/main.7cc02306.chunk.js"
+  },
+  {
+    "revision": "4033eece4697bf895f69",
+    "url": "/wouldyourather/static/js/runtime-main.170d8c5a.js"
+  },
+  {
+    "revision": "651457ed87ecdd85a46836caa6dcfd09",
+    "url": "/wouldyourather/static/media/1.651457ed.jpg"
+  }
+]);
